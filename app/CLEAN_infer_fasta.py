a9c9b45ed1da96d5e819b170621fe0703698ef50
bxuecarnegie <xuebo@msu.edu>
2026-06-03 17:24:27 -0700

    Update CLEAN_infer_fasta arguments and usage

--- app/CLEAN_infer_fasta.py
+++ app/CLEAN_infer_fasta.py
@@ -8,20 +8,26 @@ def eval_parse():
     # located in ./data/[args.fasta_data].fasta
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--fasta_data', type=str)
+    # located in ./data/[args.train_data].csv
+    # located in ./data/model/[args.train_data].pth
+    parser.add_argument('-t', '--train_data', type=str, default='split100')
+    parser.add_argument('-g', '--gmm_path', type=str, defulat='./data/pretrained/gmm_ensumble.pkl')
+    parser.add_argument('-p', '--pretrained', action='store_true')
     args = parser.parse_args()
     return args
 
 
 def main():
     args = eval_parse()
-    train_data = 'split100'
+    train_data = args.train_data
+    model_name = train_data
     test_data = 'inputs/' + args.fasta_data 
     # converting fasta to dummy csv file, will delete after inference
     # esm embedding are taken care of
     prepare_infer_fasta(test_data) 
     # inferred results is in
     # results/[args.fasta_data].csv
-    infer_maxsep(train_data, test_data, report_metrics=False, pretrained=True, gmm = './data/pretrained/gmm_ensumble.pkl')
+    infer_maxsep(train_data, test_data, report_metrics=False, pretrained=args.pretrained, model_name=model_name, gmm=args.gmm_path)
     # removing dummy csv file
     os.remove("data/"+ test_data +'.csv')
     
